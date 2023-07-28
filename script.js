6f37539870d36bf15a257519d464c88ed1707b83
unknown <wilverzxc@gmail.com>
2023-07-28 17:01:04 +0800

    Added a score counter feature where the game will reset upon 3 wins or lose

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const rockBtn = document.querySelector('.rock-btn');
 const paperBtn = document.querySelector('.paper-btn');
 const scissorBtn = document.querySelector('.scissor-btn');
+const winScore = document.querySelector('.win');
+const loseScore = document.querySelector('.lose');
 
 let winCount=0;
 let loseCount=0;
-let gameCount=0;
+// let gameCount=0;
    
 function playGame(userChoice) {
     
@@ -14,26 +16,45 @@ function playGame(userChoice) {
 
     if (userChoice === computerChoice) {
         alert(`${userChoice} is tied with ${computerChoice}`)
+        winScore.removeAttribute('style');
+        loseScore.removeAttribute('style');
     }
     else if (userChoice === 'rock' && computerChoice=== 'scissors' ||
              userChoice === 'paper' && computerChoice === 'rock' ||
              userChoice === 'scissors' && computerChoice === 'paper') {
                 alert(`${userChoice} beats ${computerChoice}. You Win!`); winCount++
-             }
-    else {
-        alert (`${computerChoice} beats ${userChoice}. You lose`) ; loseCount++ }
-    gameCount++
+                winScore.setAttribute('style', 'background-color: green; color: white;')
+                winScore.innerText = `Win: ${winCount}`;
+                loseScore.removeAttribute('style');
+    }
+   
+            
+     else {
+        alert (`${computerChoice} beats ${userChoice}. You lose`) ; 
+        loseCount++
+        loseScore.setAttribute('style', 'background: red; color: black;')
+        loseScore.innerText = `Lose: ${loseCount}`
+       
     
-    if (gameCount === 5) {
+    }
+       
+
+        //  gameCount++
+    
+    if (winCount === 3 || loseCount === 3) {
         // Display the win and lose counts
         alert(`Game Over!\nWins: ${winCount}\nLoses: ${loseCount}`);
     
         // Reset win and lose counters to zero
         winCount = 0;
         loseCount = 0;
+        winScore.removeAttribute('style');
+        loseScore.removeAttribute('style');
     
         // Reset the game count to zero
-        gameCount = 0;
+        // gameCount = 0;
+        winScore.innerText="Win:"
+        loseScore.innerText="Lose:"
       }
 };
 
